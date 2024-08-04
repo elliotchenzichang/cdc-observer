5949142637788941914a5dd5cb1e1a934b75176e
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-08-04 22:29:31 +0800

    add todo for printing the CDC events in local terminal

---
+++ cdc-observer-handler.go
@@ -0,0 +1,20 @@
+package cdcobserver
+
+import (
+	"github.com/go-mysql-org/go-mysql/canal"
+	"github.com/siddontang/go-log/log"
+)
+
+type CDCObserverHandler struct {
+	canal.DummyEventHandler
+}
+
+// todo build a tool for print the CDC event in terminal in table style
+func (h *CDCObserverHandler) OnRow(e *canal.RowsEvent) error {
+	log.Infof("%s %v\n", e.Action, e.Rows)
+	return nil
+}
+
+func (h *CDCObserverHandler) String() string {
+	return "CDCObserverHandler"
+}
