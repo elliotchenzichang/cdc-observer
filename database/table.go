e8fda31b680eb9e3b4ad7d409cd7097da7064795
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-08-08 20:00:41 +0800

    add basic data types of MySQL

--- database/table.go
+++ database/table.go
@@ -1,16 +1,9 @@
 package database
 
-type DataType uint8
-
 type Table struct {
 	Fields []*Field
 }
 
 func (t *Table) AddField(name string, dataType DataType) {
 	t.Fields = append(t.Fields, &Field{Name: name, Type: dataType})
-}
-
-type Field struct {
-	Name string
-	Type DataType
 }
