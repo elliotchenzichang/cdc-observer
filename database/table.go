e0cb764467d9eaf0ee3f4bd9aef4b803154c0b69
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-08-09 20:15:44 +0800

    add basic functions of database and table builder

--- database/table.go
+++ database/table.go
@@ -4,6 +4,132 @@ type Table struct {
 	Fields map[string]*Field
 }
 
+func NewTable(name string) *Table {
+	return &Table{
+		Fields: map[string]*Field{},
+	}
+}
+
 func (t *Table) AddField(name string, dataType interface{}) {
 	t.Fields[name] = &Field{Name: name, Type: dataType}
+}
+
+type TableBuilder struct {
+	table *Table
+}
+
+func NewTableBuilder(name string) *TableBuilder {
+	return &TableBuilder{
+		table: NewTable(name),
+	}
+}
+
+func (tb *TableBuilder) AddFieldTinyInt(name string) *TableBuilder {
+	tb.table.AddField(name, new(TinyInt))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldSmallInt(name string) *TableBuilder {
+	tb.table.AddField(name, new(SmallInt))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldMediumInt(name string) *TableBuilder {
+	tb.table.AddField(name, new(MediumInt))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldInt(name string) *TableBuilder {
+	tb.table.AddField(name, new(Int))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldBigInt(name string) *TableBuilder {
+	tb.table.AddField(name, new(BigInt))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldDecimal(name string) *TableBuilder {
+	tb.table.AddField(name, new(Decimal))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldFloat(name string) *TableBuilder {
+	tb.table.AddField(name, new(Float))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldDouble(name string) *TableBuilder {
+	tb.table.AddField(name, new(SmallInt))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldDate(name string) *TableBuilder {
+	tb.table.AddField(name, new(Date))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldTime(name string) *TableBuilder {
+	tb.table.AddField(name, new(Time))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldYear(name string) *TableBuilder {
+	tb.table.AddField(name, new(Year))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldDateTime(name string) *TableBuilder {
+	tb.table.AddField(name, new(DateTime))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldTimestamp(name string) *TableBuilder {
+	tb.table.AddField(name, new(Timestamp))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldChar(name string) *TableBuilder {
+	tb.table.AddField(name, new(Char))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldVarchar(name string) *TableBuilder {
+	tb.table.AddField(name, new(Varchar))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldText(name string) *TableBuilder {
+	tb.table.AddField(name, new(Text))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldBlob(name string) *TableBuilder {
+	tb.table.AddField(name, new(Blob))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldEnum(name string) *TableBuilder {
+	tb.table.AddField(name, new(Enum))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldSet(name string) *TableBuilder {
+	tb.table.AddField(name, new(Set))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldPoint(name string) *TableBuilder {
+	tb.table.AddField(name, new(Point))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldLineString(name string) *TableBuilder {
+	tb.table.AddField(name, new(LineString))
+	return tb
+}
+
+func (tb *TableBuilder) AddFieldJSON(name string) *TableBuilder {
+	tb.table.AddField(name, new(Json))
+	return tb
 }
