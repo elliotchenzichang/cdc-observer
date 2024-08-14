dd5ca73f9fd892eeccc37aa42e1d1dd9e36899cb
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2024-08-14 22:30:16 +0800

    add basic function to create ROw

--- database/row.go
+++ database/row.go
@@ -5,4 +5,9 @@ type Rows []Row
 // Row defines the real data in database,
 // and the data should be fited to the table schema definition
 type Row struct {
+	table string
+}
+
+func NewRow(tableName string) *Row {
+	return &Row{table: tableName}
 }
--- database/stucture_builder_test.go
+++ database/stucture_builder_test.go
@@ -3,8 +3,9 @@ package database
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestStructBuilder(t *testing.T) {
