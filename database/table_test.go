a9777b335c78e6c346867708a54703907dd56f1c
Elliot Chen <92676541+elliotchenzichang@users.noreply.github.com>
2025-01-09 11:42:21 +0800

    update test file

--- database/table_test.go
+++ database/table_test.go
@@ -2,7 +2,7 @@ package database
 
 import "testing"
 
-func TestNewTable(t *testing.T) {
+func TestTable_Basic(t *testing.T) {
 	table, err := NewTableBuilder("test_table_name", nil).AddFieldInt("test_field_int").Submit()
 	if err != nil {
 		t.Fatal(err)
